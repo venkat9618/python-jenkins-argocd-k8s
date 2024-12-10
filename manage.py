17bf9d7c835fe3991fa88b5509a1495bab30c31a
venkat9618 <39076009+venkat9618@users.noreply.github.com>
2024-12-10 11:38:37 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,20 +1,10 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
-import distutils
+print("Hello")
+
+print("__name__ value: ", __name__)
+
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoApp.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+    print("python main function")
 
 
 if __name__ == '__main__':
